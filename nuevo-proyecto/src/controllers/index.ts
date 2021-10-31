99775443ea3680966d23cde8228ee3ded9ea3975
Juan G <86728893+JuanOnly@users.noreply.github.com>
2021-10-30 21:02:21 -0500

    controladores de jurados

--- nuevo-proyecto/src/controllers/index.ts
+++ nuevo-proyecto/src/controllers/index.ts
@@ -2,3 +2,6 @@ export * from './ping.controller';
 export * from './jurados-area-investigacion.controller';
 export * from './jurados-evaluacion-solicitud.controller';
 export * from './evaluacion-solicitud-resultado-evaluacion.controller';
+export * from './jurados.controller';
+export * from './area-investigacion.controller';
+export * from './jurados-investigacion.controller';
